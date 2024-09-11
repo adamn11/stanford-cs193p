1e8208d6e311f12df4f4bb07cbb29daee4423d71
adamn11 <adamnguyen73@gmail.com>
2024-09-11 14:44:11 -0500

    Initial creation of project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Adam Nguyen on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
